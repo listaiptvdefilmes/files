3e14a088f14acf2f16f1fdeb7266dd2ae496af25
listaiptvdefilmes <96911201+listaiptvdefilmes@users.noreply.github.com>
2022-07-24 13:37:35 -0300

    Create script-para-contagem-de-segundos.js

---
+++ script-para-contagem-de-segundos.js
@@ -0,0 +1,23 @@
+var downloadButton = document.getElementById(&quot;download&quot;);
+var counter = 10;
+var newElement = document.createElement(&quot;p&quot;);
+  newElement.innerHTML = &quot;&lt;h1&gt;&lt;b&gt;&lt;/b&gt;&lt;/h1&gt;&quot;;
+var id;
+
+downloadButton.parentNode.replaceChild(newElement, downloadButton);
+
+function startDownload() {
+    this.style.display = &#39;none&#39;;
+    id = setInterval(function () {
+        counter--;
+        if (counter &lt; 0) {
+            newElement.parentNode.replaceChild(downloadButton, newElement);
+            clearInterval(id);
+        } else {
+            newElement.innerHTML = +counter.toString() + &quot; segundos.&quot;;
+        }
+    }, 1000);
+};
+
+var clickbtn = document.getElementById(&quot;btn&quot;);
+clickbtn.onclick = startDownload;
